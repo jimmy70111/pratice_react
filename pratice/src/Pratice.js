fcacd2d2c8d40b6085abcd33c6aac58e6498ea09
jimmy <tul70511@temple.edu>
2025-02-16 16:42:58 -0500

    pratice react

--- pratice/src/Pratice.js
+++ pratice/src/Pratice.js
@@ -1,11 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './center.css'
 const Pratice = () => {
 
+    const [count , setCount] =  useState(10);
+
+    
+
+    const Name = (prop) =>{
+        return <h1>  my name is {prop.title} </h1>
+    }
 
 
-
-
+    useEffect(() => {
+        if (count === 11) {
+            setCount(count *3);
+        } 
+    }, [count]); 
 
 
 
@@ -16,8 +27,16 @@ const Pratice = () => {
     return(
 
         <div>
+
+        <Name title= "jimmy"/>
         <h1  className=" reng"> Hello  World </h1>
-        <button className='but'>  getCounter </button>
+      
+
+        <div className='but'>
+        <button  onClick={ () => { setCount(count +1)}}>  Current Count : {count} </button>
+
+        </div>
+         
         </div>
     );
 
